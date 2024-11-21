88e410730020ca015d22909312836cd2786cdae0
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-11-21 09:09:54 +0530

    Navbar position fixed on homepage

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -3,8 +3,11 @@ import Home from "@/components/Home.jsx";
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen w-full">
+    <div className="fixed inset-0 overflow-y-auto ">
+      {/* <BackgroundBeamsWithCollision className="fixed inset-0 min-h-screen" /> */}
+      <div className="container mx-auto px-4 py-8 relative z-10 ">
       <Home />
+      </div>
     </div>
   );
 };
